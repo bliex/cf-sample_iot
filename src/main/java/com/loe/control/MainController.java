446b41ad26b21d62c0552a7eba497c85b6f62ae7
Bongsan Choi <optimique@bliex.com>
2016-11-22 04:05:05 +0900

    데모 샘플 수정.

--- src/main/java/com/loe/control/MainController.java
+++ src/main/java/com/loe/control/MainController.java
@@ -5,7 +5,6 @@ import java.nio.charset.Charset;
 
 import org.apache.http.ParseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -23,7 +22,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,17 +30,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
-public class MainController {
-
+public class MainController { 
+	@Value("${IOT_MGMT_URL}")
+	private String url ="";
+	
+	@Value("${IOT_OID}")
+    private String device_id="";
+	
+	@Value("${IOT_DKEY}")
+	private String dKey="";
+	
 	@Autowired
 	private SimpMessagingTemplate template;
 
 	private static String paser(String body,String name) throws Exception{
+		System.out.println("name " + name);
 		if(name.equals("content")){
-		
 			JSONParser jsonParser = new JSONParser();
 			JSONObject result = (JSONObject) jsonParser.parse(body);
-			JSONObject cin = (JSONObject) result.get("cin");			
+			JSONObject cin = (JSONObject) result.get("cin");
 		    return (String)cin.get("con");	
 		}
 		JSONParser jsonParser = new JSONParser();
@@ -50,54 +56,77 @@ public class MainController {
 	    JSONObject sgn = (JSONObject) result.get("sgn");
 	    JSONObject nev = (JSONObject) sgn.get("nev");
         JSONObject rep = (JSONObject) nev.get("rep");
-        return new String(Base64Utils.decodeFromString((String) rep.get("value"))); 	}
+	    JSONObject om = (JSONObject) nev.get("om");
+        if(om.get("op").toString().equals("1")){
+            return new String(Base64Utils.decodeFromString((String) rep.get("value"))); 	
+        }else{
+        	return om.get("op").toString();
+        } 
+	}
 
 	@RequestMapping(value="/dashboard", method=RequestMethod.POST)
 	@ResponseStatus(value=HttpStatus.OK)
 	public void dashboard(@RequestBody String body, @RequestHeader HttpHeaders headers) throws Exception {
 		String content = paser(paser(body,"representation"),"content");
-        System.out.println("content : " + content);
-        
-		//content = "{ \""+content.split("'")[1]+"\" :  \""+content.split("'")[3]+"\" , \""+content.split("'")[5]+"\" :  \""+content.split("'")[7]+"\" , \""+content.split("'")[9]+"\" :  \""+content.split("'")[11]+"\" }";
-        content = new String(Base64Utils.decodeFromString((String)content));
-        HttpEntity<String> entity = new HttpEntity<String>(content, headers);
-		this.template.convertAndSend("/topic/subscribe",entity);
+        System.out.println("Before base 64 : "+ content);
+        if(content.equals("4")){
+        	System.out.println("contentInstance is Deleted");
+        }else{
+        	//content = "{ \""+content.split("'")[1]+"\" :  \""+content.split("'")[3]+"\" , \""+content.split("'")[5]+"\" :  \""+content.split("'")[7]+"\" , \""+content.split("'")[9]+"\" :  \""+content.split("'")[11]+"\" }";
+            content = new String(Base64Utils.decodeFromString((String)content));
+
+            HttpEntity<String> entity = new HttpEntity<String>(content, headers);
+    		this.template.convertAndSend("/topic/subscribe",entity);
+        }
+		
 	}
 	
-/* Iolab  媛쒖냼��m2m �뚯뒪 
- 
-	@RequestMapping(value="/m2m", method=RequestMethod.POST) // �쒕쾭�먯꽌 蹂대궡���뺣낫瑜�援щ룆��
+
+ /* 마그네틱센서에서 값을 넘겨 받으면 전구를 키는 함수
+	@RequestMapping(value="/m2m", method=RequestMethod.POST) // 서버에서 보내온 정보를 구독함.
 	@ResponseStatus(value=HttpStatus.OK)
 	public void notify(@RequestBody String body, @RequestHeader HttpHeaders headers) throws Exception {
 		String content = paser(paser(body,"representation"),"content");
-	    System.out.println("content : " + content);
-	    String value = new String(Base64Utils.decodeFromString((String)content));
-	       System.out.println("value : " +value);
+	    System.out.println("sss");
+	    System.out.println("content1 : " + content);
+	    if(content.equals("4")){
+        	System.out.println("contentInstance is Deleted");
+	    }else{
+	    	 String value = new String(Base64Utils.decodeFromString(content));
+		       System.out.println("value : " +value);
+		       String light_url ="";
+			    String led_url ="";
+			    if(value.equals("1")){  //창문 OPEN
+			    	System.out.println("window OPEN");
+			    	sendMgmt(url, device_id, "switch1", "ON","switch2","ON", dKey);
+					HttpEntity<String> entity = new HttpEntity<String>(content, headers);
+					this.template.convertAndSend("/topic/subscribe2", entity);
+			    }else if(value.equals("0")){ //창문 Close
+			    	System.out.println("window CLOSE");
+			    	sendMgmt(url, device_id, "switch1", "OFF", "switch2","OFF",dKey);
+					HttpEntity<String> entity = new HttpEntity<String>(content, headers);
+					this.template.convertAndSend("/topic/subscribe2", entity);
+	    }
+	   
 	       
-	    String light_url ="";
-	    String led_url ="";
-	    if(value.equals("1")){  //李쎈Ц OPEN
-	    	System.out.println("window OPEN");
-	    	sendMgmt("http://apim.startiot.or.kr:8281/charlot/base", "88888.8888.RP05", "switch1", "ON","switch2","ON", "fc4b959205accb2638f0259a1faa9b4b");
-	    	sendMgmt("http://apim.startiot.or.kr:8281/charlot/base", "00000.0002.etc", "led", "OPEN","led2","OPEN", "3026d3880b8fc91991ff0e4ba290c0e0");
-
-	    }else if(value.equals("0")){ //李쎈Ц Close
-	    	System.out.println("window CLOSE");
-	    	sendMgmt("http://apim.startiot.or.kr:8281/charlot/base", "88888.8888.RP05", "switch1", "OFF", "switch2","OFF","fc4b959205accb2638f0259a1faa9b4b");
-	    	sendMgmt("http://apim.startiot.or.kr:8281/charlot/base", "00000.0002.etc", "led", "CLOSE","led2","CLOSE", "3026d3880b8fc91991ff0e4ba290c0e0");
-
+	   
 	    	
 	    }
 	   
 	    
-		//HttpEntity<String> entity = new HttpEntity<String>(content, headers);
-		//this.template.convertAndSend("/topic/subscribe", entity);
 	}	
-* 	
- */
-	@MessageMapping("/timeline") // �곕え �섏씠吏�줈 蹂대깂.
+*/
+	@MessageMapping("/timeline") // 데모 페이지로 보냄.
 	@SendTo("/topic/subscribe")
 	public HttpEntity<String> timeline(@RequestBody String body) throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+		HttpEntity<String> entity = new HttpEntity<String>(body, headers);
+		return entity;
+	}
+	@MessageMapping("/realwindow") // 데모 페이지로 보냄.
+	@SendTo("/topic/subscribe2")
+	public HttpEntity<String> realwindow(@RequestBody String body) throws Exception {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 		HttpEntity<String> entity = new HttpEntity<String>(body, headers);
@@ -106,14 +135,14 @@ public class MainController {
 
 	
 public void sendMgmt(String url,String deviceName, String cmdName, String cmd, String cmdName1, String cmd1, String dKey) throws ParseException, IOException{
-	 //RP05 -> �꾧뎄
+	 //RP05 -> 전구
    String desUrl = url+"/control-"+deviceName;
    System.out.println("desurl : " + desUrl);
     CloseableHttpClient httpclient = HttpClients.createDefault();
 	try {
 		HttpPut httpPut = new HttpPut(desUrl);
-		httpPut.setHeader("X-M2M-RI", "RQI0001"); // 由ы��ㅽ듃 ID
-		httpPut.setHeader("X-M2M-Origin", "/S"+deviceName); // �쒖뼱���대쫫
+		httpPut.setHeader("X-M2M-RI", "RQI0001"); // 리퀘스트 ID
+		httpPut.setHeader("X-M2M-Origin", "/S"+deviceName); // 제어자 이름
 		httpPut.setHeader("Accept", "application/json");
 		httpPut.setHeader("Authorization","Bearer "+dKey);
 		httpPut.setHeader("Content-Type","application/vnd.onem2m-res+json");		
@@ -141,29 +170,19 @@ public void sendMgmt(String url,String deviceName, String cmdName, String cmd, S
 
 }
 
-
-	@Value("${IOT_MGMT_URL}")
-	private String cseUrl;
-	
-	@Value("${IOT_OID}")
-	private String oid;
-	
-	@Value("${IOT_DKEY}")
-	private String dkey;
-
 @RequestMapping(value="/sendtoplug", method=RequestMethod.POST)
 @ResponseStatus(value=HttpStatus.OK)
 public void sendToplug(@RequestBody String body, @RequestHeader HttpHeaders headers) throws Exception {
 	System.out.println("in sendtoplug");  
 	System.out.println(body);
 	if(body.equals("ON")){
-		System.out.println("in ON");
-		sendMgmt(cseUrl, oid, "switch", "ON", "switch1", "null", dkey);
+		sendMgmt(url, device_id, "switch", "ON", "switch1", "null", dKey);
 	}else {
-		sendMgmt(cseUrl, oid, "switch", "OFF", "switch1", "null", dkey);
+		sendMgmt(url, device_id, "switch", "OFF", "switch1", "null", dKey);
 	}
 	
 }
+
 
 	
 }
